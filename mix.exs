951a5a903563f1dbb5f5eee39c456d5e63c1d8b2
teehuhta <teemu.huhtala@notta.fi>
2017-04-13 13:27:24 +0300

    Fixed mssqlex dependency to point at fixed version

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule MssqlEcto.Mixfile do
 
   defp deps do
     [
-      {:mssqlex, "~> 1.0.0"},
+      {:mssqlex, git: "git://github.com/ghouli/mssqlex"},
       {:ecto, "~> 2.2.0"},
       {:ex_doc, "~> 0.15", only: :dev, runtime: false},
       {:excoveralls, "~> 0.6", only: :test},
